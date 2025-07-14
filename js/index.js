75da45d3069848b6a64277399ab9007d09377130
gui-cordeiro <guisg.cordeiro@gmail.com>
2025-07-14 18:57:43 -0300

    encurtado tempo de "press and hold" da matéria

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var timer = null;               	// (internal) Timer to keep track of "click and
 var editLock = "false";				// (internal) Variable to hold if course selection is locked due to options menu
 var colorId = 0;					// (internal) Represents the index of the color being used to toggle courses as completed, as taken from the window.colors list
 var creditsOrCode = "credits"		// (internal) Holds whether credits or code are being displayed for courses at a given time
-var pressAndHoldTime = 500;    		// Period of time for the program to consider a touch/click as a "click and hold"
+var pressAndHoldTime = 250;    		// Period of time for the program to consider a touch/click as a "click and hold"
 var courseId = 1; //ID of the choosen course
 
 /* Initialization function: */
